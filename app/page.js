71fe87f99da97e8bf354b92e2f0d54d5d2984b34
Sameeksha <sameekshagopinath2006@gmail.com>
2025-11-10 00:16:19 +0530

    basic Home page content

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="py-20">
+      <div className="max-w-6xl mx-auto px-4 text-center">
+        <h1 className="text-4xl font-bold mb-4">Home Page</h1>
+      </div>
     </div>
   );
 }
