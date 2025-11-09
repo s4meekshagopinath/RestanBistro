97067de7024095c3bce35b4b5b143c955f10864d
Sameeksha <sameekshagopinath2006@gmail.com>
2025-11-09 23:08:50 +0530

    Add Navbarand fix Contact page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,10 +3,11 @@ import Link from 'next/link';
 
 export default function Navbar() {
     return (
-        <nav className='border-b'>
+        <nav className='border-b bg-white text-black'>
             <div className='max-w-6xl mx-auto flex items-center justify-between p-4'>
-                <Link href="/" className='font-semibold'>Restan Bistro</Link>
-                <div className='flex gap-4 text-sm'>
+                <Link href="/" className='font-semibold text-lg'>Restan Bistro</Link>
+                <div className='flex items-center gap-4'>
+                    <Link href='/' className='text-sm text-black'>Home</Link>
                     <Link href='/menu'>Menu</Link>
                     <Link href='/about'>About</Link>
                     <Link href='/contact'>Contact Us</Link>
