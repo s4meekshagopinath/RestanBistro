67edf0860eb1834ab132019b955bb8926093c837
Sameeksha <sameekshagopinath2006@gmail.com>
2025-11-10 00:33:45 +0530

    t

--- app/page.js
+++ app/page.js
@@ -1,8 +1,19 @@
 export default function Home() {
   return (
-    <div className="py-20">
+    <div className="pt-16">
       <div className="max-w-6xl mx-auto px-4 text-center">
-        <h1 className="text-4xl font-bold mb-4">Home Page</h1>
+        <h1 className="text-4xl font-semibold mb-3">Restan Bistro</h1>
+
+        <p className="text-base mb-6">A A cozy dining experience with delicious flavors and aesthetic ambience</p>
+
+        <div className="flex justifyicenter gap-4">
+          <a href="/menu" className="px-5 py-2 bg-blacl text-white rounded-md">
+            View Menu
+          </a>
+          <a href="/contact" className="px-5 py-2 border border-black rounded-md">
+            Book Table
+          </a>
+        </div>
       </div>
     </div>
   );
